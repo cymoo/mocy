cb27c62b60ddb37118b2ad7233af00a92c34d6fb
cymoo <milkywayjc@hotmail.com>
2021-06-06 01:19:21 +0800

    fix bugs about open useragents.txt

--- mocy/middlewares/random_useragent.py
+++ mocy/middlewares/random_useragent.py
@@ -1,7 +1,10 @@
+import os
 import random
 
+dirname = os.path.dirname(os.path.abspath(__file__))
+
 # The useragent list was taken from: https://techblog.willshouse.com/2012/01/03/most-common-user-agents/
-with open('useragents.txt', 'rt') as fp:
+with open(os.path.join(dirname, 'useragents.txt'), 'rt') as fp:
     useragents = [line.strip() for line in fp.readlines()]
 
 
